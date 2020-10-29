5b9dfa7e6a39f4cf44898baaba2f979a87d2f754
Matt Farrow <matt@formalizedcuriosity.net>
2020-10-29 11:20:26 -0500

    Add additional data exploration code

--- scripts/mf_initial_eda.R
+++ scripts/mf_initial_eda.R
@@ -1,9 +1,14 @@
+#######################################
+# Bank Marketing Data Set
+
+# https://archive.ics.uci.edu/ml/datasets/bank%20marketing#
+#######################################
 
 # Setup -------------------------------------------------------------------
 
 library(tidyverse)
 library(here)       # relative location references
-library(janitor)
+library(janitor)    # data cleanup tools
 library(naniar)     # dealing with missing values
 
 
@@ -18,7 +23,22 @@ bank <- clean_names(bank)
 
 # Examine Data ------------------------------------------------------------
 
+# What are the dimensions of the data
+dim(bank)
+
+# Look at the first and last rows of data
+head(bank)
+tail(bank)
 glimpse(bank)
+
+# Summary statistics
+summary(bank)
+skimr::skim(bank)
+
+# Other possible functions for examining the data that may be helpful
+Hmisc::describe(bank_clean)
+psych::describe(bank_clean)
+DataExplorer::create_report(bank)
 
 # Deal with Missing Values ------------------------------------------------
 
@@ -26,9 +46,24 @@ glimpse(bank)
 bank_clean <- bank %>% 
   replace_with_na_all(condition = ~ .x == "unknown")
 
-# Count number of mising values in data set
+# Convert 999 in pdays to NA
+# 999 means client was not previously contacted
+bank_clean <- bank_clean %>% 
+  replace_with_na(replace = list(pdays = 999))
+
+# Count number of missing values in data set
 colSums(is.na(bank_clean))
 
 # Other variables that may have missing values that we should explore
-unique(bank_clean$pdays)
 unique(bank_clean$poutcome)
+
+# Examine the Data (Post-Cleanup) -----------------------------------------
+
+# Re-run reports with missing values added
+DataExplorer::create_report(bank_clean)
+
+# Reports from inspectdf (categorical variables and Pearson correlation
+# coefficients)
+inspectdf::inspect_cat(bank_clean)
+inspectdf::inspect_cor(bank_clean)
+
